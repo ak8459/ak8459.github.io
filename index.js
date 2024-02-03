158c2efc4d4552c2e6917b477bfa9d8a3d35e98a
Akash Sharma <87300147+ak8459@users.noreply.github.com>
2024-02-03 22:10:45 +0530

    update resume

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function linkClick() {
 }
 
 function downloadResume() {
-    window.location.href = "https://drive.google.com/file/d/1wvZcj1aU9Hm6J2BxiPX1N5UWo_k9EwHt/view?usp=drive_link";
+    window.location.href = "https://shorturl.at/biyB6";
 }
 
 
